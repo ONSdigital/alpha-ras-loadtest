64db8b5abf8f26dd5e13daf7c37cf131d5c4f91c
Charles <enquiries@movingdigi.com>
2017-01-25 11:05:49 +0000

    auto format

--- src/test/scala/demo/loginSimulation.scala
+++ src/test/scala/demo/loginSimulation.scala
@@ -3,24 +3,35 @@
 
 package demo
 
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
+
 
 class loginSimulation extends Simulation {
 
 
   // Suck environmental vars
-  val environment = if(System.getProperty("ENVIRONMENT") != null) System.getProperty("ENVIRONMENT") else "Playpen"
-  val domain = if(System.getProperty("DOMAIN") != null) System.getProperty("DOMAIN") else "localhost"
+  val environment = if (System.getProperty("ENVIRONMENT") != null)
+    System.getProperty("ENVIRONMENT") else "Playpen"
+
+  val domain = if (System.getProperty("DOMAIN") != null)
+    System.getProperty("DOMAIN") else "localhost"
 
   // Hopefully we have been passed something that will convert to an integer
-  val port = if(System.getProperty("PORT") != null) System.getProperty("PORT").toInt else 8080
+  val port = if (System.getProperty("PORT") != null)
+    System.getProperty("PORT").toInt else 8080
 
-  val users = if(System.getProperty("USERS") != null) System.getProperty("USERS").toInt else 500
-  val seconds = if(System.getProperty("SECONDS") != null) System.getProperty("SECONDS").toInt else 10
+  val users = if (System.getProperty("USERS") != null)
+    System.getProperty("USERS").toInt else 500
 
-  val mins = if(System.getProperty("MINUTES") != null) System.getProperty("MINUTES").toInt else 1
+  val seconds = if (System.getProperty("SECONDS") != null)
+    System.getProperty("SECONDS").toInt else 10
+
+  val mins = if (System.getProperty("MINUTES") != null)
+    System.getProperty("MINUTES").toInt else 1
 
   before {
     println("Simulation is about to start!")
@@ -33,20 +44,17 @@ class loginSimulation extends Simulation {
   //
   val conf = http.baseURL("http://%s:%s".format(domain, port))
 
-  val scn = scenario("Gatling")
-    .during(30 seconds) {
-      exec(http("index2")
-             .get("/service-instances/microservice")
-             .check(regex("MICROSERVICE").find(1).exists)
-             .check(regex("vevedvcadscwdqssSs").notExists))
-      }
-      .exec(http("index1").get("/").check(status.is(session => 200)))
+  val scn = scenario("Gatling").during(30 seconds) {
+    exec(http("index2")
+      .get("/service-instances/microservice")
+      .check(regex("MICROSERVICE").find(1).exists)
+      .check(regex("vevedvcadscwdqssSs").notExists))
+  }
+    .exec(http("index1").get("/").check(status.is(session => 200)))
 
 
-
-  setUp(scn.inject(atOnceUsers(users) ))
+  setUp(scn.inject(atOnceUsers(users)))
     .protocols(conf)
 
-      //  setUp(login.inject(constantUsersPerSec(users) during(seconds)).protocols(httpProtocol))
 }
 
